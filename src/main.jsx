4f040a911a740caf48cdfbf441bfd46c13f6dfee
tdreams <fabriels@gmail.com>
2023-06-20 02:29:45 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import { ProductProvider } from "./Context/context";
-import { CartProvider } from "./Context/cart_context";
+
+import { CartProvider } from "./context/cart_context";
+import { ProductProvider } from "./context/context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
